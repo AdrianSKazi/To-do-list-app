7d0c780a848e18794a35fee1c7ec76ffe7e074a1
Kazi <adrian.kazi@accenture.com>
2022-11-30 01:52:08 +0100

    Adding Work and About pages

--- app.js
+++ app.js
@@ -4,40 +4,49 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const date = require(__dirname + "/date.js");
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'));
 
 app.set('view engine', "ejs");
 
-let items = [];
+const items = [];
+const workItems = [];
 
 app.get('/', (req, res) => {
 
-  let today = new Date();
-
-  let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'short',
-  };
-
-  let day = today.toLocaleDateString('en-US', options);
+  // const day = date.getDay();
+  const day = date.getDate();
 
   res.render("list", {
-    day_ejs : day,
+    list_title_ejs : day,
     items_ejs : items
   });
-
 });
 
 app.post('/', (req, res) => {
+  const item = req.body.newItem;
 
-  let item = req.body.newItem;
+  if (req.body.list === 'Work') {
+    workItems.push(item);
+    res.redirect('/work');
+  }
+  else {
+    items.push(item);
+    res.redirect('/');
+  }
+});
 
-  items.push(item)
+app.get('/work', (req, res) => {
+  res.render("list", {
+    list_title_ejs : "Work To-do",
+    items_ejs : workItems,
+  });
+})
 
-  res.redirect('/');
-
+app.get('/about', (req, res) => {
+  res.render('about')
 });
 
 app.listen(port, () => {
