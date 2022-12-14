24cda5acca206069ed04fe807e75142ce46d571e
Adrian Kazi <adriankazi@Adrians-MacBook-Pro.local>
2022-12-15 00:45:59 +0100

    adding mongoose database

--- app.js
+++ app.js
@@ -1,42 +1,129 @@
+// LIBRARIES
+
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 const app = express();
+app.set('view engine', "ejs");
 const port = 3000;
 
-const date = require(__dirname + "/date.js");
+
+
+
+// APP
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'));
 
-app.set('view engine', "ejs");
 
-const items = [];
-const workItems = [];
+
+
+
+
+
+// Mongoose
+
+mongoose.connect('mongodb://localhost:27017/todolistDB');
+
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+
+const task1 = new Item({
+  name: "Clean the room"
+});
+
+const task2 = new Item({
+  name: "Do dinner"
+});
+
+const task3 = new Item({
+  name: "Watch movie"
+});
+
+const items = [task1, task2, task3];
+
+
+
+
+
+
+
+
+// GET
 
 app.get('/', (req, res) => {
 
   // const day = date.getDay();
-  const day = date.getDate();
+  // const day = date.getDate();
 
-  res.render("list", {
-    list_title_ejs : day,
-    items_ejs : items
+  Item.find({}, function(err, foundItems){
+
+    if (foundItems.length === 0) {
+
+        Item.insertMany(items, function(err){
+          if (err){
+            console.log('Error on inserting');
+          } else {
+            console.log('Success on inserting');
+          }
+        });
+
+        res.redirect('/');
+
+    } else {
+
+        res.render("list", {
+        list_title_ejs : "Today",
+        items_ejs : foundItems
+
+      });
+    }
   });
 });
 
-app.post('/', (req, res) => {
-  const item = req.body.newItem;
 
-  if (req.body.list === 'Work') {
-    workItems.push(item);
-    res.redirect('/work');
-  }
-  else {
-    items.push(item);
-    res.redirect('/');
-  }
+
+
+// POST
+
+app.post('/', (req, res) => {
+
+  // const item = new Item({
+  //   name: req.body.newItem
+  // });
+
+  // if (req.body.list === 'Work') {
+  //   items.push(item);
+  //   res.redirect('/work');
+  // }
+  // else {
+
+  const item = new Item({name: req.body.newItem}, (err) => {
+    if (err){
+      console.log('Error on item push');
+    } else {
+      console.log('Success on item push');
+    }
+  });
+
+  item.save();
+
+  res.redirect('/');
+  // }
 });
+
+
+
+
+// GET WORK
+// const items = [];
+const workItems = [];
 
 app.get('/work', (req, res) => {
   res.render("list", {
@@ -45,9 +132,18 @@ app.get('/work', (req, res) => {
   });
 })
 
+
+
+
+// GET ABOUT
+
 app.get('/about', (req, res) => {
   res.render('about')
 });
+
+
+
+// LISTEN
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
