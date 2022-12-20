abc3d1837c7cdb9a3116688ca0b513f885538ce9
Adrian Kazi <adriankazi@Adrians-MacBook-Pro.local>
2022-12-20 10:22:31 +0100

    Add cool face API

--- app.js
+++ app.js
@@ -10,15 +10,6 @@ const app = express();
 app.set('view engine', "ejs");
 const PORT = process.env.PORT || 3000;
 
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
 // APP
 
 app.use(bodyParser.urlencoded({ extended: true }))
